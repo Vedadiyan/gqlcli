51cfae817e673c23911fff00f6a133dbe2fa8286
Pouya Vedadiyan <vedadiyan@gmail.com>
2023-12-08 14:56:48 +0800

    configuration support

--- cmd/main.go
+++ cmd/main.go
@@ -7,20 +7,21 @@ import (
 
 	"github.com/redis/go-redis/v9"
 	flaggy "github.com/vedadiyan/flaggy/pkg"
+	"github.com/vedadiyan/goal/pkg/di"
 	gqlmongo "github.com/vedadiyan/gql/pkg/functions/mongo"
 	gqlredis "github.com/vedadiyan/gql/pkg/functions/redis"
 	gql "github.com/vedadiyan/gql/pkg/sql"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/yaml.v3"
 )
 
 type Options struct {
-	Query       string  `long:"--query" short:"-q" help:"path to file containing the GQL query"`
-	Source      string  `long:"--src" short:"-s" help:"path to the data source"`
-	Destination string  `long:"--dest" short:"-d" help:"path to destination to save the result"`
-	Redis       *string `long:"--redis" short:"-r" help:"redis connection string"`
-	Mongo       *string `long:"--mongo" short:"-m" help:"mongodb connection string"`
-	Help        bool    `long:"--help" short:"-h" help:"displays help"`
+	Query          string  `long:"--query" short:"-q" help:"path to file containing the GQL query"`
+	Source         string  `long:"--src" short:"-s" help:"path to the data source"`
+	Destination    string  `long:"--dest" short:"-d" help:"path to destination to save the result"`
+	Configurations *string `long:"--conf" short:"-c" help:"additional configurations"`
+	Help           bool    `long:"--help" short:"-h" help:"displays help"`
 }
 
 func (opts Options) Run() error {
@@ -28,23 +29,55 @@ func (opts Options) Run() error {
 		flaggy.PrintHelp()
 		return nil
 	}
-	if opts.Redis != nil {
-		client := redis.NewClient(&redis.Options{
-			Addr: *opts.Redis,
-		})
-		gqlredis.RegisterConManager(func(connKey string) (*redis.Client, error) {
-			return client, nil
-		})
-	}
-	if opts.Mongo != nil {
-		client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(*opts.Mongo))
+	if opts.Configurations != nil {
+		bytes, err := os.ReadFile(*opts.Configurations)
 		if err != nil {
-			panic(err)
+			return err
 		}
-		gqlmongo.RegisterConManager(func(connKey string) (*mongo.Client, error) {
-			return client, nil
-		})
+		conf := make(map[string]map[string]string)
+		err = yaml.Unmarshal(bytes, &conf)
+		if err != nil {
+			return err
+		}
+		redisConf, ok := conf["redis"]
+		if ok {
+			for key, value := range redisConf {
+				client := redis.NewClient(&redis.Options{
+					Addr: value,
+				})
+				di.AddSinletonWithName[*redis.Client](key, func() (instance *redis.Client, err error) {
+					return client, nil
+				})
+			}
+			gqlredis.RegisterConManager(func(connKey string) (*redis.Client, error) {
+				redisClient, err := di.ResolveWithName[*redis.Client](connKey, nil)
+				if err != nil {
+					return nil, err
+				}
+				return *redisClient, nil
+			})
+		}
+		mongoConf, ok := conf["mongo"]
+		if ok {
+			for key, value := range mongoConf {
+				client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(value))
+				if err != nil {
+					panic(err)
+				}
+				di.AddSinletonWithName[*mongo.Client](key, func() (instance *mongo.Client, err error) {
+					return client, nil
+				})
+			}
+			gqlmongo.RegisterConManager(func(connKey string) (*mongo.Client, error) {
+				mongoClient, err := di.ResolveWithName[*mongo.Client](connKey, nil)
+				if err != nil {
+					return nil, err
+				}
+				return *mongoClient, nil
+			})
+		}
 	}
+
 	src, err := os.ReadFile(opts.Source)
 	if err != nil {
 		return err
--- go.mod
+++ go.mod
@@ -2,11 +2,15 @@ module github.com/vedadiyan/gqlcli
 
 go 1.20
 
-replace "github.com/vedadiyan/gql" => "../gql"
+replace github.com/vedadiyan/gql => ../gql
 
 require (
+	github.com/redis/go-redis/v9 v9.3.0
 	github.com/vedadiyan/flaggy v0.0.0-20221219094954-d106d0dc1b71
+	github.com/vedadiyan/goal v0.2.6
 	github.com/vedadiyan/gql v0.1.0
+	go.mongodb.org/mongo-driver v1.11.4
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -15,19 +19,17 @@ require (
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/uuid v1.4.0 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/redis/go-redis/v9 v9.3.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/vedadiyan/sqlparser v0.0.0-20230215084946-7cbf4d107dab // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.11.4 // indirect
-	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/text v0.3.8 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 )
